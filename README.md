6ce4e7cf0dfc42ce536c2fe942eaab887c93a24a
itemNo143 <kzitemno143@gmail.com>
2020-07-11 04:16:08 +0900

    修正

--- README.md
+++ README.md
@@ -1,41 +1,53 @@
-# ChatSpace DB設計
+# ChatSpace DB 設計
 
-## groups_usersテーブル
-|Column|Type|Options|
-|------|----|-------|
-|user_id|integer|null: false, foreign_key: true|
-|group_id|integer|null: false, foreign_key: true|
+## groups_users テーブル
+
+| Column   | Type    | Options                        |
+| -------- | ------- | ------------------------------ |
+| user_id  | integer | null: false, foreign_key: true |
+| group_id | integer | null: false, foreign_key: true |
+
 ### Association
+
 - belongs_to :group
 - belongs_to :user
 
-## usersテーブル
-|Column|Type|Options|
-|------|----|-------|
-|email|string|null: false|
-|password|string|null: false|
-|name|string|null: false|
+## users テーブル
+
+| Column   | Type   | Options     |
+| -------- | ------ | ----------- |
+| email    | string | null: false |
+| password | string | null: false |
+| name     | string | null: false |
+
 ### Association
+
 - has_many :messages
 - has_many :groups_users
 - has_many :groups, through: :groups_users
 
-## messagesテーブル
-|Column|Type|Options|
-|------|----|-------|
-|image|text||
-|text|text||
-|user_id|integer|null: false, foreign_key: true|
-|group_id|integer|null: false, foreign_key: true|
+## messages テーブル
+
+| Column   | Type    | Options                        |
+| -------- | ------- | ------------------------------ |
+| image    | text    |                                |
+| text     | text    |                                |
+| user_id  | integer | null: false, foreign_key: true |
+| group_id | integer | null: false, foreign_key: true |
+
 ### Association
+
 - belongs_to :user
 - belongs_to :group
 
-## groupsテーブル
-|Column|Type|Options|
-|------|----|-------|
-|name|string|null: false|
+## groups テーブル
+
+| Column | Type   | Options     |
+| ------ | ------ | ----------- |
+| name   | string | null: false |
+
 ### Association
+
 - has_many :messages
 - has_many :groups_users
 - has_many :users, through: :groups_users
--- app/assets/javascripts/modules/users.js
+++ app/assets/javascripts/modules/users.js
@@ -1,5 +1,5 @@
-$(function() {
-  function addUser(user){
+$(function () {
+  function addUser(user) {
     let html = `
                 <div class="ChatMember clearfix">
                   <p class="ChatMember__name">${user.name}</p>
@@ -8,8 +8,8 @@ $(function() {
                 `;
     $("#UserSearchResult").append(html);
   }
-  
-  function addNoUser(){
+
+  function addNoUser() {
     let html = `
                 <div class="ChatMember clearfix">
                   <p class="ChatMember__name">ユーザーが見つかりません</p>
@@ -29,37 +29,37 @@ $(function() {
     $(".ChatMembers").append(html);
   }
 
-  $("#UserSearch__field").on("keyup", function() {
+  $("#UserSearch__field").on("keyup", function () {
     let input = $("#UserSearch__field").val();
     $.ajax({
       type: "GET",
       url: "/users",
       data: { keyword: input },
-      dataType: "json"
+      dataType: "json",
     })
-    .done(function(users) {
-      $("#UserSearchResult").empty();
-      if (users.length !== 0) {
-        users.forEach(function(user) {
-          addUser(user);
-        });
-      } else if (input.length == 0) {
-        return false;
-      } else {
-        addNoUser();
-      }
-    })
-    .fail(function() {
-      alert("通信エラーです。ユーザーが表示できません。");
-    });
+      .done(function (users) {
+        $("#UserSearchResult").empty();
+        if (users.length !== 0) {
+          users.forEach(function (user) {
+            addUser(user);
+          });
+        } else if (input.length == 0) {
+          return false;
+        } else {
+          addNoUser();
+        }
+      })
+      .fail(function () {
+        alert("通信エラーです。ユーザーが表示できません。");
+      });
   });
-  $("#UserSearchResult").on("click", ".ChatMember__add", function() {
+  $("#UserSearchResult").on("click", ".ChatMember__add", function () {
     const userName = $(this).attr("data-user-name");
     const userId = $(this).attr("data-user-id");
     $(this).parent().remove();
     addMember(userName, userId);
   });
-  $(".ChatMembers").on("click", ".ChatMember__remove", function() {
+  $(".ChatMembers").on("click", ".ChatMember__remove", function () {
     $(this).parent().remove();
   });
 });
